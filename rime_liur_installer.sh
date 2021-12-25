27abf4bef2eae07e28252c79ac07908986f7b9ff
windclara@gmail.com <windclara@gmail.com>
2021-12-26 02:04:08 +0800

    調整文字

--- rime_liur_installer.sh
+++ rime_liur_installer.sh
@@ -4,9 +4,9 @@ echo "     +---------------------------+"
 echo "     | RIME 嘸蝦米一鍵安裝小工具 |"
 echo "     +---------------------------+"
 echo "請注意！若曾經安裝過，此工具會直接覆蓋掉設定檔"
-read -p "繼續安裝，請輸入Y/y" YN
+read -p "請輸入Y/y以繼續安裝。或其他任意文字，Enter後取消：" YN
 case $YN in
     [Yy]* ) curl -fsSL https://raw.githubusercontent.com/hsuanyi-chou/rime-liur/master/do_install_rime_liur.sh | bash;;
-    [Nn]* ) exit;;
+    * ) exit;;
 esac
 echo
