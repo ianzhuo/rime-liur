df6edf76858f5b9255abae8e76a2abc7ad53b459
windclara@gmail.com <windclara@gmail.com>
2021-12-26 01:57:34 +0800

    更新sh連結

--- rime_liur_installer.sh
+++ rime_liur_installer.sh
@@ -6,7 +6,7 @@ echo "     +---------------------------+"
 echo "請注意！若曾經安裝過，此工具會直接覆蓋掉設定檔"
 read -p "繼續安裝，請輸入Y/y" YN
 case $YN in
-    [Yy]* ) curl -fsSL https://github.com/hsuanyi-chou/rime-liur/blob/master/do_install_rime_liur.sh | bash;;
+    [Yy]* ) curl -fsSL https://raw.githubusercontent.com/hsuanyi-chou/rime-liur/master/do_install_rime_liur.sh | bash;;
     [Nn]* ) exit;;
 esac
 echo
