d117a35c896108dc190bd0c93e7d75b944dd6d25
windclara@gmail.com <windclara@gmail.com>
2021-12-26 01:53:53 +0800

    增加一鍵安裝shell

---
+++ rime_liur_installer.sh
@@ -0,0 +1,12 @@
+# created by Ryan Chou
+# more info: https://blog.typeart.cc/rime-liur/guide/
+echo "     +---------------------------+"
+echo "     | RIME 嘸蝦米一鍵安裝小工具 |"
+echo "     +---------------------------+"
+echo "請注意！若曾經安裝過，此工具會直接覆蓋掉設定檔"
+read -p "繼續安裝，請輸入Y/y" YN
+case $YN in
+    [Yy]* ) curl -fsSL https://github.com/hsuanyi-chou/rime-liur/blob/master/do_install_rime_liur.sh | bash;;
+    [Nn]* ) exit;;
+esac
+echo
