ffc746ffca36f41bcbde8ee13c2d38fa42e60f7a
windclara <windclara@gmail.com>
2021-12-28 14:43:50 +0800

    增加自動部署功能

--- rime_liur_installer.sh
+++ rime_liur_installer.sh
@@ -6,31 +6,45 @@ RIME_LIUR_FOLDER=$RIME_FOLDER/rime-liur-master
 echo "     +---------------------------+"
 echo "     | RIME 嘸蝦米自動安裝小工具 |"
 echo "     +---------------------------+"
-echo "請注意！本工具會透過 plum 初始化「預設所有輸入法」與下載並覆蓋現有的「嘸蝦米輸入法」"
-echo "若有自己的客制化版本尚未備份，請盡速按下 ctrl + c 終止作業！"
+echo "請注意！本工具將會執行以下作業："
+echo "1. 透過 plum 初始化「預設所有輸入法」"
+echo "2. 初始化「嘸蝦米輸入法」(若已存在則覆蓋)"
+echo "3. 部署 RIME (安裝所有輸入法)"
+echo
+echo "※ 若 macOS 有開啟「勿擾模式」，將看不到 RIME 部署成功的通知，建議先關閉「勿擾模式」以掌握安裝進度"
+echo "※ 若有自己的客制化版本尚未備份，請盡速按下 ctrl + c 終止作業！"
+echo
 for i in {5..1};
 do
     echo "將在 $i 秒後開始作業…";
     sleep 1
 done
+echo
 echo "+------------------------------------+"
 echo "| step 1: 使用 plum 初始化注音輸入法 |"
 echo "+------------------------------------+"
 curl -fsSL https://git.io/rime-install | bash
 echo
-echo "+--------------------------------+"
-echo "| step 2: 下載 RIME 嘸蝦米輸入法 |"
-echo "+--------------------------------+"
+echo "+----------------------------------+"
+echo "| step 2: 初始化 RIME 嘸蝦米輸入法 |"
+echo "+----------------------------------+"
 cd $RIME_FOLDER && curl -L https://github.com/hsuanyi-chou/rime-liur/archive/refs/heads/master.zip | tar xz
 cp -R $RIME_LIUR_FOLDER/Rime/* $RIME_FOLDER
 rm -rf $RIME_LIUR_FOLDER
 echo
-echo "     +---------------------------------+"
-echo "     |嘸蝦米 與 注音輸入法 初始化完成！|"
-echo "     +---------------------------------+"
+echo "+-----------------------+"
+echo "| step 3: 自動部署 RIME |"
+echo "+-----------------------+"
+/Library/Input\ Methods/Squirrel.app/Contents/MacOS/Squirrel --reload
 echo
-echo "請點擊右上角 鼠鬚管「㞢」 -> 用戶設定(會打開資料夾) -> 打開「liur_customWords.dict.yaml」設定加字加詞"
-echo "或不設定，直接點擊右上角 鼠鬚管「㞢」 -> 重新部署 直接安裝嘸蝦米!"
-echo "若之後再異動加字加詞，亦請按下「重新部署」以生效加字加詞"
+echo "     +------------------------+"
+echo "     | RIME 嘸蝦米 安裝完成 ✨ |"
+echo "     +------------------------+"
 echo
-echo "更多資訊請參考 https://blog.typeart.cc/rime-liur/guide/"
+echo "設定加字加詞方式："
+echo "1. 點擊右上角 鼠鬚管「㞢」"
+echo "2. 點擊「用戶設定」(會打開資料夾)"
+echo "3. 打開「liur_customWords.dict.yaml」輸入加字加詞"
+echo "4. 點右上角 鼠鬚管「㞢」，按下「重新部署」生效加字加詞"
+echo
+echo "更多資訊請參考：https://blog.typeart.cc/rime-liur/guide/"
