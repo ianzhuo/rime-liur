f260b06952e0dd81ef5a740b94e7170a68755583
windclara@gmail.com <windclara@gmail.com>
2022-01-28 23:16:31 +0800

    增加檢查鼠鬚管主程式是否存在

--- rime_liur_installer.sh
+++ rime_liur_installer.sh
@@ -3,6 +3,8 @@
 # more info: https://blog.typeart.cc/rime-liur/guide/
 RIME_FOLDER="$HOME"/Library/Rime
 RIME_LIUR_FOLDER=$RIME_FOLDER/rime-liur-master
+SQUIRREL_APP=/Library/Input\ Methods/Squirrel.app/Contents/MacOS/Squirrel
+
 echo "     +---------------------------+"
 echo "     | RIME 嘸蝦米自動安裝小工具 |"
 echo "     +---------------------------+"
@@ -14,6 +16,12 @@ echo
 echo "※ 若 macOS 有開啟「勿擾模式」，將看不到 RIME 部署成功的通知，建議先關閉「勿擾模式」以掌握安裝進度"
 echo "※ 若有自己的客制化版本尚未備份，請盡速按下 ctrl + c 終止作業！"
 echo
+if [ ! -f "$SQUIRREL_APP" ]; then
+    echo "檢查到尚未安裝鼠鬚管！終止作業"
+    echo "請先至鼠鬚管官方網站下載並安裝鼠鬚管後，再次執本工具"
+    echo "鼠鬚管官方網站下載主程式：https://rime.im/download/"
+    exit
+fi
 for i in {5..1};
 do
     echo "將在 $i 秒後開始作業…";
@@ -35,7 +43,7 @@ echo
 echo "+-----------------------+"
 echo "| step 3: 自動部署 RIME |"
 echo "+-----------------------+"
-/Library/Input\ Methods/Squirrel.app/Contents/MacOS/Squirrel --reload
+"$SQUIRREL_APP" --reload
 echo
 echo "     +------------------------+"
 echo "     | RIME 嘸蝦米 安裝完成 ✨ |"
